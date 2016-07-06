fc099bf3a39aef78b2843a73a9707fe22892d32c
Santosh Singh <santosh.singh@surya-solutions.com>
2016-07-06 19:32:29 +0100

    comments:

--- push-run-debug.sh
+++ push-run-debug.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -a -m "comments: $1"
 git push
-ssh santosh@rackspace "cd /home/santosh/homebee/homebee-server-app;/home/santosh/homebee/homebee-server-app/pull-debug.sh"
+ssh santosh@rackspace "cd /home/santosh/homebee/homebee-server-app;git reset --hard;git pull"
